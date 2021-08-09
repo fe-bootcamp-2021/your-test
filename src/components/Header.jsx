1dfeb50bd65249c854b334d9fc6b589db45dbc5b
Vahe1209 <Vahe1209@gmail.com>
2021-08-09 16:08:39 +0400

    (fix): tailwind config

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <button className="px-5 py-5 border border-red-500">Sign In</button>
+      <button className="px-5 py-5 border border-red-500">Sign Up</button>
+    </div>
+  );
+}
