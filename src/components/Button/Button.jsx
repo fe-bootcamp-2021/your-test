69f7248ac2ff106f0b53aa7afedf5863b2644747
Vahe1209 <Vahe1209@gmail.com>
2021-08-09 17:22:46 +0400

    (feat): add new components Header, Button, HomePage

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classNames from "classnames";
+
+export default function Button({ value, color }) {
+  const buttonStyle = classNames(
+    "px-5",
+    "py-5",
+    `border-${color}-500`,
+    `bg-${color}-400`,
+    "rounded-xl",
+    "md:w-40"
+  );
+  return <button className={buttonStyle}>{value}</button>;
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "../Button/Button";
+
+export default function Header() {
+  return (
+    <div>
+      <Button value="Sign In" color="red" />
+      <Button value="Sign Up" color="green" />
+    </div>
+  );
+}
