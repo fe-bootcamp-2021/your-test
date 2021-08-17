7eb6858f8debe64039862fc5b996f4dec9adf218
Anri <anrihunanyan@gmail.com>
2021-08-17 18:30:55 +0400

    feat(QuetionForm.jsx): created quetion form component

---
+++ src/components/QuetionForm/QuetionForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+
+const quetionSection = classNames();
+
+export default function QuetionForm() {
+  const [quetion, setQuetion] = useState("");
+
+  return (
+    <div className="fixed top-1/3 w-screen flex flex-col justify-center items-center">
+      <div className="w-1/2 heading text-center font-bold text-2xl m-5 text-gray-800 flex justify-center items-center ">
+        Create quetion
+      </div>
+      <div className="w-1/2 editor mx-auto flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl bg-gray-200">
+        <Input
+          spellCheck="false"
+          placeholder="Quetion"
+          onChange={(e) => {
+            setQuetion(e.target.value);
+          }}
+          value={quetion}
+          type="text"
+        />
+
+        <div className="buttons flex">
+          <Button
+            value="Cancel"
+            // onClick={}
+          />
+          <Button
+            value="Create"
+            // onClick={}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
