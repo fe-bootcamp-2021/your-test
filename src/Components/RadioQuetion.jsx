ac26ff88f3241a2a480be1ec451e15a2fff5598c
Anri <anrihunanyan@gmail.com>
2021-08-24 20:35:54 +0400

    feat(RadioQuetion.jsx): created point functionality for radio type answer

--- src/Components/RadioQuetion.jsx
+++ src/Components/RadioQuetion.jsx
@@ -23,10 +23,19 @@ export default function RadioQuetion({ setAnswerSection, answerSection }) {
     });
   };
 
+  const handlePoint = (val) => (e) => {
+    console.log(val);
+    setAnswerSection((prevState) => {
+      return {
+        ...prevState,
+        point: e.target.value,
+      };
+    });
+  };
+
   return (
     <>
       {answerSection.answer.map((e, i) => {
-        console.log(e);
         return (
           <div>
             <Input name="asdd" value={e} type="radio" />
@@ -35,7 +44,19 @@ export default function RadioQuetion({ setAnswerSection, answerSection }) {
         );
       })}
       <Button buttonName="Add" onClick={addAnswer} />
-
+      <span>Point</span>
+      <select value={answerSection.point} onChange={handlePoint("point")}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
     </>
   );
 }
