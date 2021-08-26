53389a7d58f4c97ed77945a34f9ef5c114e1aec5
dav26072000 <dav260720000@mail.ru>
2021-08-26 16:26:49 +0300

    feat(testPaper):add question add popup to test paper

--- src/components/TestPaper.jsx
+++ src/components/TestPaper.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+import Button from "./Button";
+import PlusIcon from "./icons/PlusIcon";
+import QuestionForm from "./QuestionForm";
+
 export default function TestPaper({ testId, testInfo }) {
+  const [isQuestionForm, setIsQuestionForm] = useState(false);
+
   return (
-    <div
-      className="w-1/2 border-solid border-2 border-gray-200 shadow-xl flex items-center flex-col p-4"
-      style={{ minHeight: "95%" }}
-    >
-      <h1 className="text-4xl">{testInfo.testTitle}</h1>
-      <p className="text-lg">{testInfo.testDescription}</p>
-    </div>
+    <>
+      {isQuestionForm && <QuestionForm closeQuestionForm={setIsQuestionForm} />}
+      <div
+        className="w-1/2 border-solid border-2 border-gray-200 shadow-xl flex items-center flex-col p-4"
+        style={{ minHeight: "95%" }}
+      >
+        <h1 className="text-4xl">{testInfo.testTitle}</h1>
+        <p className="text-lg">{testInfo.testDescription}</p>
+        <div className="w-full">
+          <Button
+            buttonName={<PlusIcon />}
+            width="12"
+            color="blue"
+            style={{
+              borderRadius: "100px",
+              height: "3rem",
+              padding: "0",
+            }}
+            onClick={() => {
+              setIsQuestionForm(true);
+            }}
+          />
+        </div>
+      </div>
+    </>
   );
 }
