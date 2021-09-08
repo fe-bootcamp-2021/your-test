3d1f9559fabaf250f9671de3cb4c32faa633bd22
Vahe1209 <Vahe1209@gmail.com>
2021-09-08 15:24:38 +0400

    feat(UserProfile.jsx): add new feature delete test

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -36,32 +36,49 @@ export default function UserProfile() {
   return (
     <div className="flex" style={{ minHeight: "92vh" }}>
       {messagePopup && <Popup {...messagePopup} />}
-      <div className="min-h-full border-solid border-2 border-gray-100 w-2/12 shadow-xl">
+      <div className="min-h-full border-solid border-2 border-gray-100 w-96 shadow-xl">
         <Button buttonName="Create Test" color="green" onClick={handleClick} />
         <div>{currentUser && currentUser.email}</div>
         {Object.keys(currentUserTests).length > 0
           ? Object.values(currentUserTests).map((elem) => (
-              <Button
-                buttonName={elem[1].testTitle}
-                color="blue"
-                key={elem[0]}
-                onClick={(e) => {
-                  e.preventDefault();
-                  Promise.resolve(true)
-                    .then(() => {
-                      setTestPaperData((prev) => ({
-                        ...prev,
-                        testId: elem[0],
-                        testInfo: elem[1],
-                      }));
+              <div>
+                <Button
+                  buttonName={elem[1].testTitle}
+                  color="blue"
+                  key={elem[0]}
+                  width="11/12"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    Promise.resolve(true)
+                      .then(() => {
+                        setTestPaperData((prev) => ({
+                          ...prev,
+                          testId: elem[0],
+                          testInfo: elem[1],
+                        }));
 
-                      return true;
-                    })
-                    .then(() => {
-                      setIsTestPaper(true);
-                    });
-                }}
-              />
+                        return true;
+                      })
+                      .then(() => {
+                        setIsTestPaper(true);
+                      });
+                  }}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 inline-block md:from-blue-500 md:hover:from-blue-700"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              </div>
             ))
           : null}
       </div>
