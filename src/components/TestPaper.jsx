468d228a0230964bd9bbb8482a3d80f07bbf7efe
Anri <anrihunanyan@gmail.com>
2021-09-10 04:41:19 +0400

    fix(TestPaper.jsx): corrected popup position

--- src/components/TestPaper.jsx
+++ src/components/TestPaper.jsx
@@ -14,7 +14,7 @@ export default function TestPaper({ testId, testInfo }) {
   const [isQuestionForm, setIsQuestionForm] = useState(false);
   const [allQuestions, setAllQuestions] = useState([]);
 
-  const [showPCopyPopup, setShowCopyPopup] = useState({
+  const [showCopyPopup, setShowCopyPopup] = useState({
     isPopup: false,
     massage: "",
     isError: false,
@@ -23,7 +23,6 @@ export default function TestPaper({ testId, testInfo }) {
 
   useEffect(() => {
     getTestQuestions({ testId }).then((res) => {
-      console.log(res);
       setAllQuestions(res);
     });
     setIsQuestionForm(false);
@@ -32,84 +31,85 @@ export default function TestPaper({ testId, testInfo }) {
   return (
     <>
       <Popup
-        message={showPCopyPopup.massage}
-        isError={showPCopyPopup.isError}
-        isPopup={showPCopyPopup.isPopup}
+        message={showCopyPopup.massage}
+        isError={showCopyPopup.isError}
+        isPopup={showCopyPopup.isPopup}
         showPopup={setShowCopyPopup}
-        setTimer={showPCopyPopup.setTimer}
+        setTimer={showCopyPopup.setTimer}
       />
-      {isQuestionForm && (
+      {isQuestionForm ? (
         <QuestionForm
           closeQuestionForm={setIsQuestionForm}
           testId={testId}
           setAllQuestions={setAllQuestions}
         />
-      )}
-      <div
-        className="w-1/2 border-solid border-2 border-gray-200 shadow-xl flex items-center flex-col p-4"
-        style={{ minHeight: "95%" }}
-      >
-        <h1 className="text-4xl">{testInfo.testTitle}</h1>
-        <p className="text-lg">{testInfo.testDescription}</p>
-        <div className="w-full flex justify-between">
-          <Button
-            buttonName={<PlusIcon />}
-            width="12"
-            color="blue"
-            style={{
-              borderRadius: "100px",
-              height: "3rem",
-              padding: "0",
-            }}
-            onClick={() => {
-              setIsQuestionForm(true);
-            }}
-          />
-          <div className="flex">
+      ) : (
+        <div
+          className="w-1/2 border-solid border-2 border-gray-200 shadow-xl flex items-center flex-col p-4"
+          style={{ minHeight: "95%" }}
+        >
+          <h1 className="text-4xl">{testInfo.testTitle}</h1>
+          <p className="text-lg">{testInfo.testDescription}</p>
+          <div className="w-full flex justify-between">
+            <Button
+              buttonName={<PlusIcon />}
+              width="12"
+              color="blue"
+              style={{
+                borderRadius: "100px",
+                height: "3rem",
+                padding: "0",
+              }}
+              onClick={() => {
+                setIsQuestionForm(true);
+              }}
+            />
+            <div className="flex">
+              {allQuestions.length > 0 && (
+                <Button
+                  buttonName={<CopyLinkIcon />}
+                  width="12"
+                  color="gray"
+                  style={{
+                    borderRadius: "100px",
+                    height: "3rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    color: "white",
+                  }}
+                  onClick={() => {
+                    if (
+                      navigator.clipboard.writeText(
+                        `${webPageRoute}${testsPageRoute}${testId}`
+                      )
+                    ) {
+                      setShowCopyPopup({
+                        isPopup: true,
+                        massage: "Copied",
+                        isError: false,
+                        setTimer: 1000,
+                      });
+                    } else {
+                      setShowCopyPopup({
+                        isPopup: true,
+                        massage: "Can not copy",
+                        isError: true,
+                        setTimer: 1000,
+                      });
+                    }
+                  }}
+                />
+              )}
+            </div>
+          </div>
+          <div className="w-full mt-10">
             {allQuestions.length > 0 && (
-              <Button
-                buttonName={<CopyLinkIcon />}
-                width="12"
-                color="gray"
-                style={{
-                  borderRadius: "100px",
-                  height: "3rem",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  color: "white",
-                }}
-                onClick={() => {
-                  if (
-                    navigator.clipboard.writeText(
-                      `${webPageRoute}${testsPageRoute}${testId}`
-                    )
-                  ) {
-                    setShowCopyPopup({
-                      isPopup: true,
-                      massage: "Copied",
-                      isError: false,
-                      setTimer: 1000,
-                    });
-                  } else {
-                    setShowCopyPopup({
-                      isPopup: true,
-                      massage: "Can not copy",
-                      isError: true,
-                      setTimer: 1000,
-                    });
-                  }
-                }}
-              />
+              <TestPaperQuestions allQuestions={allQuestions} />
             )}
           </div>
         </div>
-        <div className="w-full mt-10">
-          {allQuestions.length > 0 && (
-            <TestPaperQuestions allQuestions={allQuestions} />
-          )}
-        </div>
-      </div>
+      )}
     </>
   );
 }
