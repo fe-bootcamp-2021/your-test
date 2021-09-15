4ae1c7caedce4eec17aef936a22c9995522b007e
Anri <anrihunanyan@gmail.com>
2021-09-15 12:25:47 +0400

    feat(Results.jsx): createdresults component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,134 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-export default function Results() {
-  return <div>Hi Abo</div>;
+import { getCurrentResults, getResults } from "../services/results.services";
+import Loader from "./Loader";
+import Input from "./Input";
+import Button from "./Button";
+
+export default function Results({ testId, testInfo }) {
+  const [currentResults, setCurrentResults] = useState([
+    {
+      answer: ["fvsdzgrtg", "vdgrt"],
+      correctAnswer: ["vdgrt"],
+      point: 0,
+      question: "vsgrd",
+      questionId: "-MjWkN5Qyow52S0inXFZ",
+      selected: ["fvsdzgrtg"],
+      type: "checkbox",
+    },
+    {
+      answer: ["fvsdzgrtg", "vdgrt"],
+      correctAnswer: "vdgrt",
+      point: 5,
+      question: "vsgrd",
+      questionId: "-MjWkN5Qyow52S0inXFZ",
+      selected: "vdgrt",
+      type: "radio",
+    },
+  ]);
+  const [results, setResults] = useState([
+    { email: "adasf@fs.fd", date: "2021,12,21", resultId: "fsawr4" },
+    { email: "adasf@fs.fd", date: "2021,12,21", resultId: "fsawr3" },
+    { email: "adasf@fs.fd", date: "2021,12,21", resultId: "fs57r4" },
+  ]);
+
+  useEffect(() => {
+    getResults({ testId }).then((res) => {
+      setResults(res);
+      console.log(res);
+    });
+  }, [testId, currentResults, results]);
+
+  const handleCurrentResults = (resultId) => {
+    // alert(resultId);
+    getCurrentResults(resultId).then((res) => {
+      setCurrentResults(res);
+    });
+  };
+
+  if (!results) {
+    return <Loader />;
+  }
+  return (
+    <div
+      className="w-96 m-auto border-solid border-2 border-gray-200 shadow-xl flex flex-col p-4"
+      style={{ minHeight: "95%" }}
+    >
+      <h1 className="text-4xl">{testInfo.testTitle}</h1>
+      <p className="text-lg">{testInfo.testDescription}</p>
+
+      {currentResults ? (
+        <div>
+          <Button
+            buttonName="Back"
+            color="red"
+            onClick={() => {
+              setCurrentResults(false);
+            }}
+          />
+          {currentResults.map((obj, i) => {
+            return (
+              <div>
+                <hr />
+                <div className="p-2 flex justify-between">
+                  <p className="font-semibold">
+                    <span>{i + 1}. </span>
+                    {obj.question}
+                  </p>
+                  {obj.point}
+                </div>
+
+                {obj.type === "text" ? (
+                  <Input
+                    name={i}
+                    disabled
+                    placeholder="Enter text"
+                    type={obj.type}
+                    value={obj.selected}
+                  />
+                ) : (
+                  obj.answer.map((el) => {
+                    return (
+                      <label htmlFor={i} className="block ml-6">
+                        <Input
+                          disabled
+                          name={i}
+                          checked={
+                            obj.type === "radio"
+                              ? el === obj.correctAnswer
+                              : obj.correctAnswer
+                              ? obj.correctAnswer.find((a) => a === el)
+                              : false
+                          }
+                          type={obj.type}
+                          value={el}
+                        />
+                        {el}
+                      </label>
+                    );
+                  })
+                )}
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        results.map((e) => {
+          return (
+            <div
+              className="w-full cursor-pointer border-2 border-gray-200 p-2 m-2 flex justify-between"
+              key={e.resultId}
+              onClick={() => {
+                handleCurrentResults(e.resultId);
+              }}
+              role="presentation"
+            >
+              <span>{e.email}</span>
+              <span>{e.date}</span>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 }
